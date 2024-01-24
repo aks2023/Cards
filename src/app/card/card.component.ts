cfb4e53cc69cba72e5e8db82e6ed04e25f6528c9
aks2023 <aks2023@hotmail.com>
2024-01-24 16:20:11 +0530

    Took data from parent component and completed Ui

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrl: './card.component.css'
+  styleUrl: './card.component.css',
 })
 export class CardComponent {
+  @Input() title = '';
+  @Input() imageUrl = '';
+  @Input() content = '';
+  @Input() username = '';
 
+  constructor() {}
+
+  ngOnInit() {}
 }
